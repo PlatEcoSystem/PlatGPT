64e49a032a78b1068301f560257f1eefcc3c9029
arseniyvizgalov <arseniyvizgalov@gmail.com>
2025-07-07 23:27:26 +0300

    набросок бота

--- tgbot/platon.py
+++ tgbot/platon.py
@@ -1,5 +1,29 @@
-import telebot
+from aiogram import Dispatcher, Bot
+from aiogram.filters import CommandStart
+from aiogram.types  import Message
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+bot = Bot(bot_token)
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет!')
+
+if __name__ == '__main__':
+    asyncio.run(main())
 
 
-bot =
+
+
 
